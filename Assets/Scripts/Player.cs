fab9b44c0a7ea707b853826ab40e0d41fc35a845
Carmen Zhuang <carmenz@sfu.ca>
2017-05-29 17:03:02 -0700

    game board

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	int currentPosition;
+	int targetPosition;
+	string info;
+
+	int dice = 5;
+
+	// Use this for initialization
+	void Start () {
+		currentPosition = 0;
+		Move(dice);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Move(int num) {
+		StartCoroutine(MoveTo(num));
+	}
+
+
+	IEnumerator MoveTo(int num) {
+		
+		for (int i = 0; i < num; i++) {
+			var nextSquare = BoardManager.Squares [currentPosition + 1];
+
+			float currentx = this.transform.position.x;
+			float currenty = this.transform.position.y;
+
+			this.transform.Translate(new Vector2(nextSquare.transform.position.x - currentx + 1, nextSquare.transform.position.y - currenty));
+
+			currentPosition++;
+			yield return new WaitForSeconds (1);
+		}
+
+
+	}
+
+	void UpdateTargetPosition() {
+		targetPosition = currentPosition + dice;
+	}
+
+	void OnLanding() {
+
+	}
+
+
+
+	void UpdateScore() {
+
+	}
+}
---
+++ Assets/Scripts/SquareController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System;
+
+public class SquareController : MonoBehaviour {
+
+	public string type;
+	public int index;
+	// Use this for initialization
+	void Awake () {
+		index = Int32.Parse(Regex.Replace(this.gameObject.name, @"[^\d\d]", ""));
+		//print (index);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
