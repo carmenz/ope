a5e6137d846ffc0d5c76d044ce3765cf8d9fb8ff
allisonnn <chenxiaoo@outlook.com>
2017-05-31 12:50:37 -0700

    linked

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -5,11 +5,14 @@ using DG.Tweening;
 
 
 public class SpinWheel : MonoBehaviour {
+	GameObject player;
 
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
 
 	// Use this for initialization
 	void Start () {
-		RandomFunc();
 	}
 	
 	// Update is called once per frame
@@ -17,17 +20,20 @@ public class SpinWheel : MonoBehaviour {
 
 	}
 
-	void RandomFunc() {
-		int num = (int)Random.Range (1f, 7f);
-		//print (num);
+	void Move(int chance) {
+		player.GetComponent<Player>().Move(chance);
+	}
 
-	
+	public void RandomFunc() {
+		int num = (int)Random.Range (1f, 7f);
+		print(num);
+		
 		int val = 3600 + (int)Random.Range ((num - 1) * 60f + 3, 60f * num - 3);
 
 		Sequence spinSequence = DOTween.Sequence ();  
 		spinSequence.SetEase (Ease.OutCirc)
-		.Append (transform.DORotate (new Vector3 (0, 0, val), 3, RotateMode.FastBeyond360));
-
+			.Append (transform.DORotate (new Vector3 (0, 0, val), 3, RotateMode.FastBeyond360))
+			.OnComplete(() => Move(num));
 	}
 
 
--- Assets/Scripts/SquareController.cs
+++ Assets/Scripts/SquareController.cs
@@ -6,16 +6,16 @@ using System;
 
 public class SquareController : MonoBehaviour {
 
-    public string type;
-    public int index;
-    // Use this for initialization
-    void Awake () {
-        index = Int32.Parse(Regex.Replace(this.gameObject.name, @"[^\d\d]", ""));
-    }
-    
-    // Update is called once per frame
-    void Update () {
-        
-    }
+	public string type;
+	public int index;
+	// Use this for initialization
+	void Awake () {
+		index = Int32.Parse(Regex.Replace(this.gameObject.name, @"[^\d\d]", ""));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
 }
