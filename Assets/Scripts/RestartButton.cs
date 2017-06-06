37583f950f88f018fd8f963704eeca7bbfc97289
allisonnn <chenxiaoo@outlook.com>
2017-06-06 11:44:14 -0700

    add restart button

---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour {
+
+	GameManager gm;
+
+	void Awake () {
+		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RestartGame() {
+		// Reset state
+		gm.currentPosition = 0;
+
+		// Reload main scene
+		SceneManager.LoadScene("Allison-ChangeWheel");
+	}
+}
