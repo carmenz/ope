c1c03b6081f5ce1fb87ab517e16b9759fe9b7553
Carmen Zhuang <carmenz@sfu.ca>
2017-06-08 16:04:59 -0700

    storing user

---
+++ Assets/Scripts/database/SaveLoad.cs
@@ -0,0 +1,26 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic; 
+//using System.Runtime.Serialization.Formatters.Binary; 
+//using System.IO;
+//
+//public static class SaveLoad {
+//	public static UserContainer userContainer = new UserContainer();
+//}
+
+//public static void Save() {
+//	savedUsers.Add(User.current);
+//	BinaryFormatter bf = new BinaryFormatter();
+//	FileStream file = File.Create (Application.persistentDataPath + "/savedUsers.xml");
+//	bf.Serialize(file, SaveLoad.savedUsers);
+//	file.Close();
+//}
+//
+//public static void Load() {
+//	if(File.Exists(Application.persistentDataPath + "/savedUsers.xml")) {
+//		BinaryFormatter bf = new BinaryFormatter();
+//		FileStream file = File.Open(Application.persistentDataPath + "/savedUsers.xml", FileMode.Open);
+//		SaveLoad.savedGames = (List<User>)bf.Deserialize(file);
+//		file.Close();
+//	}
+//}
---
+++ Assets/Scripts/database/UserContainer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+
+
+
+[XmlRoot("UserCollection")]
+public class UserContainer
+{
+	[XmlArray("Users"),XmlArrayItem("User")]
+	public List<UserData> users = new List<UserData>();
+
+
+	public void Save(string path)
+	{
+		
+		var serializer = new XmlSerializer(typeof(UserContainer));
+		//Debug.Log("UserContainer Save");
+		using(var stream = new FileStream(path, FileMode.Create))
+		{
+			serializer.Serialize(stream, this);
+		}
+
+	}
+
+	public static UserContainer Load(string path)
+	{
+		var serializer = new XmlSerializer(typeof(UserContainer));
+		using(var stream = new FileStream(path, FileMode.Open))
+		{
+			return serializer.Deserialize(stream) as UserContainer;
+		}
+	}
+}
+
