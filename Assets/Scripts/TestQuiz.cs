d1d4c3092e9b6c0269d4652eed988d3bc2c0a0b0
allisonnn <chenxiaoo@outlook.com>
2017-06-26 11:11:16 -0700

    add video mechainc

--- Assets/Scripts/TestQuiz.cs
+++ Assets/Scripts/TestQuiz.cs
@@ -9,18 +9,18 @@ public class TestQuiz : MonoBehaviour {
 	GameManager gm;
 
 	// Use this for initialization
-	void Start () {
-		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-	}
+	// void Start () {
+	// 	gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+	// }
 	
-	// Update is called once per frame
-	void Update () {
+	// // Update is called once per frame
+	// void Update () {
 		
-	}
+	// }
 
-	public void QuitQuiz() {
-		gm.missions.quizRequired.Remove(gm.typeCode);
-		gm.typeCode = 0;
-		SceneManager.LoadScene(mainSceneName);
-	}
+	// public void QuitQuiz() {
+	// 	gm.missions.quizRequired.Remove(gm.typeCode);
+	// 	gm.typeCode = 0;
+	// 	SceneManager.LoadScene(mainSceneName);
+	// }
 }
