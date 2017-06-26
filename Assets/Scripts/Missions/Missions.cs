e7f9becd7eb5352ac40aba9a3d94139e38bbfc21
allisonnn <chenxiaoo@outlook.com>
2017-06-26 10:07:25 -0700

    add more lists

--- Assets/Scripts/Missions/Missions.cs
+++ Assets/Scripts/Missions/Missions.cs
@@ -6,4 +6,13 @@ using UnityEngine;
 public class Missions {
 	public List<int> quizRequired = new List<int>();
 	public string quizInfo;
+
+	public List<int> storyRequired = new List<int>();
+	public string storyInfo;
+
+	public List<int> gameRequired = new List<int>();
+	public string gameInfo;
+
+	public List<int> itemsRequired = new List<int>();
+	public string itemInfo;
 }
