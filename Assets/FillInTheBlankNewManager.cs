080df51e58c32816729bb93c675e70ac641bcee1
allisonnn <chenxiaoo@outlook.com>
2017-07-19 17:12:02 -0700

    fill in the blank

---
+++ Assets/FillInTheBlankNewManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Xml;
+using System;
+using DG.Tweening;
+using System.Text.RegularExpressions;
+
+public class FillInTheBlankNewManager : MonoBehaviour {
+
+	static string userpath = string.Empty;
+	int NUM_OF_OPTIONS = 4;
+	string CORRECT_COLOR = "#008000ff";
+	string WRONG_COLOR = "#ff0000ff";
+	public int CHANCE = 2;
+
+	GameManager gm;
+	XmlDocument xmlFillInTheBlankDoc;
+	Text questionTextBox;
+	List<Option> options = new List<Option>();
+	Text infoTextBox;
+	Text scoreBox;
+
+	XmlNode fib;
+	string _fibIndex;
+	XmlNodeList _questions;
+	int _curQuestionIndex = 0;
+	string _lastAnswer;
+
+	// Use this for initialization
+	void Start () {
+		// Load data
+		gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+		userpath = System.IO.Path.Combine (Application.dataPath, "Resources/users.xml");
+		
+		xmlFillInTheBlankDoc = new XmlDocument ();
+		xmlFillInTheBlankDoc.Load (gm.Path);
+		fib = xmlFillInTheBlankDoc.SelectSingleNode("//FIB");
+		_fibIndex = xmlFillInTheBlankDoc.SelectSingleNode("//FIB/@index").Value;
+		_questions = fib.ChildNodes;
+		
+		XmlDocument xmlUserDoc = new XmlDocument ();
+		xmlUserDoc.Load (userpath);
+		XmlNode usernameNode = xmlUserDoc.SelectSingleNode ("//Username");
+		
+		// TODO: set up boxes
+		questionTextBox = GameObject.Find ("Question").GetComponent<Text> ();
+		for (var i=1;i<=NUM_OF_OPTIONS;i++) {
+			options.Add(GameObject.Find ("Option" + i.ToString()).GetComponent<Option> ());
+		}
+		infoTextBox = GameObject.Find ("Info").GetComponent<Text> ();
+		scoreBox = GameObject.Find("Score").GetComponent<Text>();
+
+		// init first line
+		RenderNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RenderNext () {
+		// reset
+		CHANCE = 2;
+
+		XmlNode questionNode = _questions.Item(_curQuestionIndex);
+
+		//check if there is a next line
+		if (questionNode == null)
+			return;
+
+		// TODO: push next line into text box
+		var questionText = questionNode.SelectSingleNode(".//Value").InnerText;
+		questionTextBox.text = questionTextBox.text + questionText;
+
+		// Init the options
+		var optionsNode = questionNode.SelectSingleNode(".//Options").ChildNodes;
+		for (var i=0; i< optionsNode.Count;i++) {
+			options[i].gameObject.SetActive(true);
+			options[i].Init(optionsNode.Item(i));
+		}
+
+		_curQuestionIndex++;
+	}
+
+	public void RenderAnswerIntoGameView(string answer, bool isCorrect) {
+		var text = questionTextBox.text;
+		var color = isCorrect ? CORRECT_COLOR : WRONG_COLOR;
+
+		var reg = new Regex(@"________________");
+
+		if (reg.IsMatch(text)) {
+			questionTextBox.text = reg.Replace(text, "<color=" + color + ">" + answer + "</color>");
+		}
+
+	}
+
+	public void RenderInfo(string info) {
+		infoTextBox.text = info;
+	}
+
+	public void AddScore() {
+		// TODO: add score
+		// TODO: animation of +10
+		// TODO: animation of scoreTextBox
+	}
+
+	public void ShowPanel() {
+		// TODO: set score text box
+		// TODO: set active
+	}
+
+}
--- Assets/Scripts/FillInTheBlank/Option.cs
+++ Assets/Scripts/FillInTheBlank/Option.cs
@@ -1,29 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Xml;
+using System;
+using DG.Tweening;
 
 public class Option : MonoBehaviour {
 
-	private FillInTheBlankManager fibm;
+	private FillInTheBlankNewManager fibm;
+
+	[SerializeField]
+	int optionIndex;
+	string value;
+	bool correctAnswer;
+	string info;
+	XmlNode root;
 
 	void Start() {
-		fibm = GameObject.Find("FillInTheBlankManager").GetComponent<FillInTheBlankManager>();
+		fibm = GameObject.Find("FillInTheBlankManager").GetComponent<FillInTheBlankNewManager>();
 	}
 
-	public void onClick1() {
-		fibm.TaskOnClick(1);
+	public void Init(XmlNode optionNode) {
+		root = optionNode;
+		value = root.SelectSingleNode(".//Value").InnerText;
+		info = root.SelectSingleNode(".//Info").InnerText;
+		correctAnswer = root.SelectSingleNode(".//Correct").InnerText == "T" ? true : false;
+		Render();
 	}
 
-	public void onClick2() {
-		fibm.TaskOnClick(2);
+	public void Render() {
+		gameObject.transform.Find("Text").GetComponent<Text>().text = value;
 	}
 
-	public void onClick3() {
-		fibm.TaskOnClick(3);
-	}
-
-	public void onClick4() {
-		fibm.TaskOnClick(4);
+	public void OnClick() {
+		if (correctAnswer) {
+			// got correct answer
+			// TODO: add score
+			// TODO: check chance
+			//fibm.AddScore(第一次就正确的分数or第二次正确的分数);
+			fibm.RenderAnswerIntoGameView(value, correctAnswer);
+			fibm.RenderInfo("That's correct! " + info);
+			fibm.RenderNext();
+		} else {
+			fibm.CHANCE--;
+			// check chance if chance - 1 > 0
+			if (fibm.CHANCE > 0) {
+				// alert first wrong
+				fibm.RenderInfo("Ooh, that's incorrect. Try again!");
+				// disable this button
+				gameObject.SetActive(false);
+			} else {
+				// alert correct answer
+				// TODO: add score
+				//fibm.AddScore(不正确加不加分？);
+				fibm.RenderAnswerIntoGameView(value, correctAnswer);
+				fibm.RenderInfo("That's still incorrect. " + info);
+				fibm.RenderNext();
+			}
+		}
 	}
 }
